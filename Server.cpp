0c074061b5737743582fc51a649442d14dc708f2
w-phenix <50770563+w-phenix@users.noreply.github.com>
2021-07-02 10:48:23 +0800

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -1,5 +1,12 @@
 #include "HttpWebServer.h"
 #include <cstdio>
+#include <iostream>
+#include <pthread.h>
+
+void* AcceptRequest(void* s){
+    HttpWebServer *server = static_cast<HttpWebServer*>(s);
+    server->HandleRequest();
+}
 
 int main(){
     HttpWebServer server;
@@ -7,11 +14,16 @@ int main(){
     server.InitServer(port);
     printf("httpd running on port %d\n", port);
     sockaddr_in clientaddr{};
+    pthread_t newThread;
     while (true){
         clientaddr = server.Accept();
         printf("New connection... ip: %s, port: %d\n",
                inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
-        server.HandleRequest();
+        // 启动线程处理新的连接
+        if(pthread_create(&newThread, NULL, AcceptRequest, &server) != 0){
+            perror("thread_create");
+        }
+        std::cout << "thread_id: " << newThread << std::endl << std::endl;
     }
     return 0;
 }
